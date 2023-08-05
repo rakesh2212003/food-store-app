8218e5e09ea0c48448e757ea62518c1ec66a0b20
Rakesh Rana <rakesh.rana2212003@gmail.com>
2023-08-05 20:44:37 +0530

    complete authentication

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
 import './App.css';
+import { Login, Main } from './containers'
 
 function App() {
     return (
-        <div className="App">
-            <h1>Hello</h1>
+        <div className='w-screen min-h-screen h-auto flex flex-col items-center justify-center'>
+            <Routes>
+                <Route path='/*' element={<Main />} />
+                <Route path='/login' element={<Login />} />
+            </Routes>
         </div>
     );
 }
--- server/functions/index.js
+++ server/functions/index.js
@@ -1,19 +1,30 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const functions = require('firebase-functions') 
+const admin = require('firebase-admin')
+const dotenv = require('dotenv')
+const express = require('express')
+const cors = require('cors')
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+const serviceAccountKey = require('./serviceAccountKey.json')
+const userRoute = require('./routes/user')
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+dotenv.config()
+const app = express()
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+app.use(express.json())
+app.use(cors({ origin: true }))
+app.use((req, res, next) => {
+    res.set("Access-Control-Allow-Origin", '*')
+    next()
+})
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccountKey)
+});
+
+app.get('/', (req, res) => {
+    return res.send('Hello World')
+})
+
+app.use('/api/users', userRoute)
+
+exports.app = functions.https.onRequest(app);
